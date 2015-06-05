3bb462cf0667fcc8b9b96462e72c04a74706511f
Marlon Santos <marlonsantos.au@gmail.com>
2015-06-05 09:20:21 -0400

    Adding analytics

---
+++ analytics/authorization.js
@@ -0,0 +1,9 @@
+io.configure(function () {
+    io.set('authorization', function (handshakeData, callback) {
+        if (handshakeData.xdomain) {
+            callback('Cross-domain connections are not allowed');
+        } else {
+            callback(null, true);
+        }
+    });
+});
---
+++ analytics/server.js
@@ -0,0 +1,28 @@
+var io = require('socket.io').listen(3000),
+	util = require('util');
+
+/*io.configure(function () {
+    io.set('authorization', function (handshakeData, callback) {
+        if (handshakeData.xdomain) {
+            callback('Cross-domain connections are not allowed');
+        } else {
+            callback(null, true);
+        }
+    });
+});*/
+
+io.sockets.on('connection', function (socket) {
+ 
+    socket.on('message', function (message) {
+        console.log("Got message: " + message);
+        ip = socket.handshake.address.address;
+        url = message;
+        io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length, 'ip': '***.***.***.' + ip.substring(ip.lastIndexOf('.') + 1), 'url': url, 'xdomain': socket.handshake.xdomain, 'timestamp': new Date()});
+    });
+ 
+    socket.on('disconnect', function () {
+        console.log("Socket disconnected");
+        io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length});
+    });
+ 
+});
